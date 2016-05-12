a5313f370e25ba0bd3cb37577194f56f01725e2c
Lukas Galke <git@lpag.de>
2016-05-12 11:09:36 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ echo "[vi-fi] Installing other Plugins"
 echo | vim +PluginInstall +qall 2>/dev/null
 # create symlinks
 echo "[vi-fi] Creating symlinks"
-ln -snv $VIFI_DIR/vimrc ~/.vimrc
-ln -snv $VIFI_DIR/vimfiles ~/.vim
+ln -snv "$@" $VIFI_DIR/vimrc ~/.vimrc
+ln -snv "$@" $VIFI_DIR/vimfiles ~/.vim
