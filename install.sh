1d04bf03df4f8dd029a0c9fba313073bf2dc3965
Lukas Galke <git@lpag.de>
2016-05-12 11:00:20 +0200

    fixy

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 VIFI_DIR=$( cd $(dirname "$0"); pwd -P)
+cd $VIFI_DIR
 echo "[vi-fi] Initializing submodule Vundle"
 git submodule init
 git submodule update
-echo "[vi-fi] Performs Vundle's PluginInstall"
+echo "[vi-fi] Installing other Plugins"
 echo | vim +PluginInstall +qall 2>/dev/null
 # create symlinks
-echo "[vi-fi] Trying to create symlinks"
+echo "[vi-fi] Creating symlinks"
 ln -snv $VIFI_DIR/vimrc ~/.vimrc
 ln -snv $VIFI_DIR/vimfiles ~/.vim
