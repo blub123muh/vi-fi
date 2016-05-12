f3c8574d18a24e4f7c75747c408851dfee031755
Lukas Galke <git@lpag.de>
2016-05-12 10:45:45 +0200

    install updated

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git submodule init
 git submodule update
-vim +PluginInstall +qall 2>/dev/null
+echo | vim +PluginInstall +qall 2>/dev/null
 # create symlinks
 DOTFILES_DIR=$( cd $(dirname "$0"); pwd -P)
 ln -snv $DOTFILES_DIR/vimrc ~/.vimrc
