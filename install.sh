2e00b90ed221cd062711bcf3442e6d6c112e892b
Lukas Galke <git@lpag.de>
2016-05-12 17:22:34 +0200

    install.sh can be also used for updating

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "[vi-fi] Initializing submodule Vundle"
 git submodule init
 git submodule update
 echo "[vi-fi] Installing other Plugins via Vundle"
-echo | vim +PluginInstall +qall 2>/dev/null
+echo | vim +PluginInstall! +qall 2>/dev/null
 # create symlinks
 echo "[vi-fi] Creating symlinks"
 ln -snv "$@" $VIFI_DIR/vimrc ~/.vimrc
