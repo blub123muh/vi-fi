0f05af7183992722898732b6d7888ac16d401bae
Lukas Galke <git@lpag.de>
2016-05-16 16:34:34 +0200

    fixed precedence of install script

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@ VIFI_DIR=$( cd $(dirname "$0"); pwd -P)
 cd $VIFI_DIR
 echo "[vi-fi] Cloning Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git $VIFI_DIR/vimfiles/bundle/Vundle.vim
-echo "[vi-fi] Installing other Plugins via Vundle"
-echo | vim +PluginInstall! +qall 2>/dev/null
-# create symlinks
 echo "[vi-fi] Creating symlinks"
 ln -snv "$@" $VIFI_DIR/vimrc ~/.vimrc
 ln -snv "$@" $VIFI_DIR/vimfiles ~/.vim
+echo "[vi-fi] Installing other plugins via Vundle"
+echo | vim +PluginInstall! +qall 2>/dev/null
