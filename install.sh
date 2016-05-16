e926888d0c932064a3d0124015c01feb992444b2
Lukas Galke <git@lpag.de>
2016-05-17 01:53:43 +0200

    little ifthenelse in install.sh for more beauty

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 VIFI_DIR=$( cd $(dirname "$0"); pwd -P)
-cd $VIFI_DIR
-echo "[vi-fi] Cloning Vundle"
-git clone https://github.com/VundleVim/Vundle.vim.git $VIFI_DIR/vimfiles/bundle/Vundle.vim
-echo "[vi-fi] Creating symlinks"
+if [ -e $VIFI_DIR/vimfiles/bundle/Vundle.vim ]
+then
+    echo "[vi-fi] Found Vundle"
+else
+    echo "[vi-fi] Cloning Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git $VIFI_DIR/vimfiles/bundle/Vundle.vim
+fi
+echo "[vi-fi] Creating symlinks (force with -f)"
 ln -snv "$@" $VIFI_DIR/vimrc ~/.vimrc
 ln -snv "$@" $VIFI_DIR/vimfiles ~/.vim
 echo "[vi-fi] Installing other plugins via Vundle"
