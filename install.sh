554d9a9da247971052e424f9ed77778279a4d40c
Lukas Galke <git@lpag.de>
2016-05-19 18:27:23 +0200

    ./install.sh is now independent of linking to ~/.vimrc

--- install.sh
+++ install.sh
@@ -8,11 +8,13 @@ VIFI_DIR=$( cd $(dirname "$0"); pwd -P)
 #fi
 if [ ! -e $VIFI_DIR/vimfiles/autoload/plug.vim ]
 then
+    echo "[vi-fi] Cloning vim-plug"
     curl -fLo $VIFI_DIR/vimfiles/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+echo "[vi-fi] Installing other plugins with vim-plug"
+echo | vim -u $VIFI_DIR/vimrc +PlugInstall +qall 2>/dev/null
 
 echo "[vi-fi] Creating symlinks (force with -f)"
 ln -snv "$@" $VIFI_DIR/vimrc ~/.vimrc
 ln -snv "$@" $VIFI_DIR/vimfiles ~/.vim
-echo "[vi-fi] Installing other plugins via vim-plug"
-echo | vim +PlugInstall +qall 2>/dev/null
