9338ca4f3d600be46dd565da40bf1473205e80aa
Lukas Galke <git@lpag.de>
2016-09-03 22:05:32 +0200

    installscript

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "[vi-fi] Creating symlinks (force with -f)"
 ln -snv "$@" $VIFI_BASE/vimrc ~/.vimrc
 ln -snv "$@" $VIFI_BASE/vim ~/.vim
 ln -snv "$@" $VIFI_BASE/gitconfig ~/.gitconfig
-# ln -snv "$@" $VIFI_BASE/zshrc ~/.zshrc
+ln -snv "$@" $VIFI_BASE/zshrc ~/.zshrc
 ln -snv "$@" $VIFI_BASE/tmux.conf ~/.tmux.conf
 
 
